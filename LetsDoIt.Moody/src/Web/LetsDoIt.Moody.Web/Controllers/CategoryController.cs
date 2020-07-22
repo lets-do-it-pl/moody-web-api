1c55252b0122f1076fae9151e56446aa294474e1
Elif Aygun <elifdilaa@gmail.com>
2020-07-22 12:56:22 +0200

    CategoryController-Added

---
+++ LetsDoIt.Moody/src/Web/LetsDoIt.Moody.Web/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsDoIt.Moody.Web.Controllers
+{
+    public class CategoryController
+    {
+    }
+}
