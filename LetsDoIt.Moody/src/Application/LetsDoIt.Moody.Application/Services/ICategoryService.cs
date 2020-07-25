74759cf128017ce9a05ca2fa6e0d60e2335f3e74
ruveydacalis <sruveydacls@gmail.com>
2020-07-25 23:29:41 +0200

    remove category services ignore unnecessary files like bin, obj, etc...

--- LetsDoIt.Moody/src/Application/LetsDoIt.Moody.Application/Services/ICategoryService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LetsDoIt.Moody.Application
-{
-    public interface ICategoryService
-    {
-        void Update(int id, string name, int order, byte[] image);
-
-        void Delete(int id, string name, int order, byte[] image);
-    }
-}
