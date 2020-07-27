a4cb7e2eb96f3ce0dfb21757606091690acb61a5
Şaban Durna <sabandurna@gmail.com>
2020-07-27 19:58:10 +0200

    resolve conflict in VersionHistory

--- LetsDoIt.Moody/src/Domain/LetsDoIt.Moody.Domain/VersionHistory.cs
+++ LetsDoIt.Moody/src/Domain/LetsDoIt.Moody.Domain/VersionHistory.cs
@@ -1,36 +1,20 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-=======
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
->>>>>>> create-update-category-API
 
 namespace LetsDoIt.Moody.Domain
 {
     public class VersionHistory
     {
-<<<<<<< HEAD
-		public VersionHistory()
-		{
-		}
-		public long Id { get; set; }
-		public string VersionNumber { get; set; }
-		public DateTime CreateDate { get; } //It should be generated automatically
-	}
-}
-=======
         [Key]        
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
 		public long Id { get; set; }
 
         [Required]
+		[StringLength(100)]
 		public string VersionNumber { get; set; }
 		
         [Required]
         public DateTime CreateDate { get; set; }
 	}
 }
->>>>>>> create-update-category-API
