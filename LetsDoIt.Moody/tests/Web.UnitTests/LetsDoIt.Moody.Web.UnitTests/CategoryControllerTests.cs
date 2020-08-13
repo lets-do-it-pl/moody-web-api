6c8db21a6ad18d9d879916463d2daf42e44e6db7
Elif Aygun <elifdilaa@gmail.com>
2020-08-13 15:15:24 +0200

    Should_BeTrimmedVersionNumber_When_VersionNumberWithSpaces

--- LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/CategoryControllerTests.cs
+++ LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/CategoryControllerTests.cs
@@ -1,99 +1,117 @@
-//using Moq;
-//using System;
-//using Xunit;
+using Moq;
+using System;
+using Xunit;
 
-//namespace LetsDoIt.Moody.Web.UnitTests
-//{
-//    using Application;
-//    using Controllers;
-//    using LetsDoIt.Moody.Application.Category;
-//    using LetsDoIt.Moody.Web.Entities.Requests;
-//    using System.Threading.Tasks;
+namespace LetsDoIt.Moody.Web.UnitTests
+{
+    using Application;
+    using Controllers;
+    using LetsDoIt.Moody.Application.Category;
+    using LetsDoIt.Moody.Web.Entities.Requests;
+    using System.Threading.Tasks;
 
-//    public class CategoryControllerTests
-//    {
-//        private readonly CategoryController _testing;
-//        private readonly Mock<ICategoryService> _mockCategoryService;
+    public class CategoryControllerTests
+    {
+        private readonly CategoryController _testing;
+        private readonly Mock<ICategoryService> _mockCategoryService;
 
-//        public CategoryControllerTests()
-//        {
-//            _mockCategoryService = new Mock<ICategoryService>();
-//            _testing = new CategoryController(_mockCategoryService.Object);
-//        }
-
-//        [Theory]
-//        [InlineData(null)]
-//        [InlineData("")]
-//        [InlineData(" ")]
-//        public async Task Insert_WhenNameIsMissing_ShouldThrownAnArgumentException(string name)
-//        {
-//            // arrange
-//            byte[] arr1 = { 0, 100, 120, 210, 255 };
-
-//            var insertRequest = new CategoryInsertRequest
-//            {
-//                Name = name,
-//                Order = 5,
-//                Image = arr1
-//            };
-
-//            // act
-//            async Task Action() => await _testing.Insert(insertRequest);
-
-//            //assert
-//            await Assert.ThrowsAsync<ArgumentException>(Action);
-//        }
+        public CategoryControllerTests()
+        {
+            _mockCategoryService = new Mock<ICategoryService>();
+            _testing = new CategoryController(_mockCategoryService.Object);
+        }
 
 
-//        [Fact]
-//        public async Task Insert_WhenImageisMissing_ShouldThrownAnArgumentException()
-//        {
-//            //arrange
-//            var insertRequest = new CategoryInsertRequest
-//            {
-//                Name = "sdfasdf",
-//                Order = 5,
-//                Image = null
-//            };
+        [Theory]
+        [InlineData("asd","asd")]
+        [InlineData("asd ", "asd")]
+        [InlineData("asd  ", "asd")]
+        [InlineData("   asd", "asd")]
+        [InlineData("  asd", "asd")]
+        public async Task Should_BeTrimmedVersionNumber_When_VersionNumberWithSpaces(string versionNumber, string expected)
+        {
 
-//            // act
-//            async Task Action() => await _testing.Insert(insertRequest);
+            //Act
+            var categories = await _testing.GetCategories(versionNumber);
 
-//            //assert
-//            await Assert.ThrowsAsync<ArgumentException>(Action);
-//        }
+            //Assert
+            _mockCategoryService.Verify(service => service.GetCategories(expected), Times.Once);
 
-//        [Fact]
-//        public async Task SaveUserAsync_ShouldSaveUserInformation()
-//        {
-//            // arrange
-//            byte[] arr1 = { 0, 100, 120, 210, 255 };
+        }
 
-//            var insertRequest = new CategoryInsertRequest
-//            {
-//                Name = "test",
-//                Order = 5,
-//                Image = arr1
-//            };
+            //[Theory]
+            //[InlineData(null)]
+            //[InlineData("")]
+            //[InlineData(" ")]
+            //public async Task Insert_WhenNameIsMissing_ShouldThrownAnArgumentException(string name)
+            //{
+            //    // arrange
+            //    byte[] arr1 = { 0, 100, 120, 210, 255 };
+
+            //    var insertRequest = new CategoryInsertRequest
+            //    {
+            //        Name = name,
+            //        Order = 5,
+            //        Image = arr1
+            //    };
+
+            //    // act
+            //    async Task Action() => await _testing.Insert(insertRequest);
+
+            //    //assert
+            //    await Assert.ThrowsAsync<ArgumentException>(Action);
+            //}
 
 
-//            _mockCategoryService.Setup(cs => 
-//                cs.InsertAsync(
-//                    insertRequest.Name, 
-//                    insertRequest.Order, 
-//                    insertRequest.Image));
+            //[Fact]
+            //public async Task Insert_WhenImageisMissing_ShouldThrownAnArgumentException()
+            //{
+            //    //arrange
+            //    var insertRequest = new CategoryInsertRequest
+            //    {
+            //        Name = "sdfasdf",
+            //        Order = 5,
+            //        Image = null
+            //    };
 
-//            // act
-//            await _testing.Insert(insertRequest);
+            //    // act
+            //    async Task Action() => await _testing.Insert(insertRequest);
 
-//            // assert
-//            _mockCategoryService.Verify(cs => 
-//                cs.InsertAsync(
-//                    insertRequest.Name,
-//                    insertRequest.Order,
-//                    insertRequest.Image), 
-//                Times.Once);
-//        }
+            //    //assert
+            //    await Assert.ThrowsAsync<ArgumentException>(Action);
+            //}
 
-//    }
-//}
+            //[Fact]
+            //public async Task SaveUserAsync_ShouldSaveUserInformation()
+            //{
+            //    // arrange
+            //    byte[] arr1 = { 0, 100, 120, 210, 255 };
+
+            //    var insertRequest = new CategoryInsertRequest
+            //    {
+            //        Name = "test",
+            //        Order = 5,
+            //        Image = arr1
+            //    };
+
+
+            //    _mockCategoryService.Setup(cs =>
+            //        cs.InsertAsync(
+            //            insertRequest.Name,
+            //            insertRequest.Order,
+            //            insertRequest.Image));
+
+            //    // act
+            //    await _testing.Insert(insertRequest);
+
+            //    // assert
+            //    _mockCategoryService.Verify(cs =>
+            //        cs.InsertAsync(
+            //            insertRequest.Name,
+            //            insertRequest.Order,
+            //            insertRequest.Image),
+            //        Times.Once);
+            //}
+
+        }
+}
