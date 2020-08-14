0523f921a76f4ade806e1d78c6f6752f75d91dc0
Saban Durna <sabandurna@gmail.com>
2020-08-14 13:36:53 +0200

    add Category Service test class

---
+++ LetsDoIt.Moody/tests/Application.UnitTests/LetsDoIt.Moody.Application.UnitTests/Category/CategoryServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+
+namespace LetsDoIt.Moody.Application.UnitTests.Category
+{
+    using Application.Category;
+    using Application.VersionHistory;
+    using Domain;    
+    using Persistance.Repositories.Base;
+
+    public class CategoryServiceTests
+    {
+        private readonly CategoryService _testing;
+
+        private readonly Mock<IEntityRepository<Category>> _mockCategoryRepository;
+        private readonly Mock<IEntityRepository<VersionHistory>> _mockVersionHistoryRepository;
+        private readonly Mock<IVersionHistoryService> _mockVersionHistoryService;
+
+        public CategoryServiceTests()
+        {
+            _mockCategoryRepository = new Mock<IEntityRepository<Category>>();
+            _mockVersionHistoryRepository = new Mock<IEntityRepository<VersionHistory>>();
+            _mockVersionHistoryService = new Mock<IVersionHistoryService>();
+
+            _testing = new CategoryService(
+                    _mockCategoryRepository.Object,
+                    _mockVersionHistoryRepository.Object,
+                    _mockVersionHistoryService.Object);
+        }
+    }
+}
