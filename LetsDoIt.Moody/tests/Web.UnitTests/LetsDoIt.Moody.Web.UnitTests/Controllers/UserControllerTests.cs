98e17e88d60b0e58c1719e8d5e9b15e7271b5392
NotMuchLeft <ahmetkuris2308@gmail.com>
2020-08-15 02:05:57 +0200

    Resolved Conflict

--- LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/Controllers/UserControllerTests.cs
+++ LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/Controllers/UserControllerTests.cs
@@ -1,17 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Data;
+﻿using System.Data;
 using System.Net;
-using System.Reflection;
 using System.Threading.Tasks;
+using LetsDoIt.Moody.Application.User;
+using LetsDoIt.Moody.Web.Controllers;
+using LetsDoIt.Moody.Web.Entities.Requests;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
 
-namespace LetsDoIt.Moody.Web.UnitTests
+namespace LetsDoIt.Moody.Web.UnitTests.Controllers
 {
-    using Application.User;
-    using Controllers;
-    using Entities.Requests;
     public class UserControllerTests
     {
         private readonly UserController _testing;
