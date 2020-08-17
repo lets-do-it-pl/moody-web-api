568aaa40e127934acaa6b6ef611760611441f20c
Emre Elbir <59312457+emreelbir@users.noreply.github.com>
2020-08-17 17:29:44 +0200

    Merge

---
+++ LetsDoIt.Moody/src/Application/LetsDoIt.Moody.Application/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿namespace LetsDoIt.Moody.Application
+{
+    public interface ICategoryService
+    {
+        void Update(int id, string name, int order, byte[] image);
+
+        void Insert( string name, int order, byte[] image);
+    }
+}
