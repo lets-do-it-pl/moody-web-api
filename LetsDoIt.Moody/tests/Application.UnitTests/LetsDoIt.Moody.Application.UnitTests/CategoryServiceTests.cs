54325863aed1d1fc0cf09fb789a3da8aa81db464
ruveydacalis <sruveydacls@gmail.com>
2020-08-20 19:21:51 +0200

    tests

---
+++ LetsDoIt.Moody/tests/Application.UnitTests/LetsDoIt.Moody.Application.UnitTests/CategoryServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using LetsDoIt.Moody.Web.Entities.Requests;
+using Moq;
+using Xunit;
+
+namespace LetsDoIt.Moody.Application.UnitTests.Category
+{
+    using Application.Category;
+    using Application.VersionHistory;
+    using Domain;
+    using Persistance.Repositories.Base;
+
+    public class CategoryServiceTests
+    {
+        private readonly CategoryService _testing;
+
+        private readonly Mock<IEntityRepository<Category>> _mockCategoryRepository;
+        private readonly Mock<IEntityRepository<VersionHistory>> _mockVersionHistoryRepository;
+        private readonly Mock<IVersionHistoryService> _mockVersionHistoryService;
+
+        public CategoryServiceTests()
+        {
+            _mockCategoryRepository = new Mock<IEntityRepository<Category>>();
+            _mockVersionHistoryRepository = new Mock<IEntityRepository<VersionHistory>>();
+            _mockVersionHistoryService = new Mock<IVersionHistoryService>();
+
+            _testing = new CategoryService(
+                    _mockCategoryRepository.Object,
+                    _mockVersionHistoryRepository.Object,
+                    _mockVersionHistoryService.Object);
+        }
+        [Fact]
+        public async Task DeleteAsync_GivenNoException_ShouldInvokeRepositoryAddAsync()
+        {
+            var request = new CategoryInsertRequest
+            {
+                Id = 1
+            };
+            await _testing.DeleteAsync(request.Id);
+            _mockCategoryRepository.Verify(ur =>
+                   ur.AddAsync(It.Is<Category>(c => c.Id == request.Id))
+               );
+
+        }
+
+        [Fact]
+        public async Task DeleteAsync_GivenNoException_ShouldInvokeVersionHistory()
+        {
+            var request = new CategoryDeleteRequest
+            {
+                Id = 1
+            };
+
+            await _testing.DeleteAsync(request.Id);
+
+            _mockVersionHistoryService.Verify(ur =>
+                ur.CreateNewVersionAsync()
+            );
+        }
+    }
+
+
+}
--- LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/CategoryControllerTests.cs
+++ LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/CategoryControllerTests.cs
@@ -1,99 +1,77 @@
-//using Moq;
-//using System;
-//using Xunit;
+using System;
+using Moq;
+using Xunit;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace LetsDoIt.Moody.Web.UnitTests
-//{
-//    using Application;
-//    using Controllers;
-//    using LetsDoIt.Moody.Application.Category;
-//    using LetsDoIt.Moody.Web.Entities.Requests;
-//    using System.Threading.Tasks;
+namespace LetsDoIt.Moody.Web.UnitTests.Controllers
+{
+    using Application.Category;
+    using Application.CustomExceptions;
+    using Web.Controllers;    
+    using Web.Entities.Requests;       
 
-//    public class CategoryControllerTests
-//    {
-//        private readonly CategoryController _testing;
-//        private readonly Mock<ICategoryService> _mockCategoryService;
+    public class CategoryControllerTests
+    {
+        private readonly CategoryController _testing;
+        private readonly Mock<ICategoryService> _mockCategoryService;
 
-//        public CategoryControllerTests()
-//        {
-//            _mockCategoryService = new Mock<ICategoryService>();
-//            _testing = new CategoryController(_mockCategoryService.Object);
-//        }
+        #region SetUp & Helpers
 
-//        [Theory]
-//        [InlineData(null)]
-//        [InlineData("")]
-//        [InlineData(" ")]
-//        public async Task Insert_WhenNameIsMissing_ShouldThrownAnArgumentException(string name)
-//        {
-//            // arrange
-//            byte[] arr1 = { 0, 100, 120, 210, 255 };
+        public CategoryControllerTests()
+        {
+            _mockCategoryService = new Mock<ICategoryService>();
+            _testing = new CategoryController(_mockCategoryService.Object);
+        }
 
-//            var insertRequest = new CategoryInsertRequest
-//            {
-//                Name = name,
-//                Order = 5,
-//                Image = arr1
-//            };
+        
+        [Fact]
+        public async Task GIVEN_ThereIsAnDeleteRequestWithoutId_WHEN_DeletingTheRequest_THEN_ShouldGetBadRequest()
+        {
+            CategoryDeleteRequest deleteRequest = null;
 
-//            // act
-//            async Task Action() => await _testing.Insert(insertRequest);
+            //Act
+            var actual = await _testing.Delete(deleteRequest);
 
-//            //assert
-//            await Assert.ThrowsAsync<ArgumentException>(Action);
-//        }
+            //Assert
+            Assert.IsType<BadRequestResult>(actual);
+        }
 
 
-//        [Fact]
-//        public async Task Insert_WhenImageisMissing_ShouldThrownAnArgumentException()
-//        {
-//            //arrange
-//            var insertRequest = new CategoryInsertRequest
-//            {
-//                Name = "sdfasdf",
-//                Order = 5,
-//                Image = null
-//            };
+        [Fact]
+        public async Task GIVEN_ThereIsAnDeleteRequest_WHEN_DeletingTheRequest_THEN_ShouldGetOkResult()
+        {
+            CategoryDeleteRequest deleteRequest = new CategoryDeleteRequest
+            {
+                Id = 1
+            };
 
-//            // act
-//            async Task Action() => await _testing.Insert(insertRequest);
+            //Act
+            var actual = await _testing.Delete(deleteRequest);
 
-//            //assert
-//            await Assert.ThrowsAsync<ArgumentException>(Action);
-//        }
+            //Assert
+            Assert.IsType<OkResult>(actual);
+        }
+        [Fact]
+        public async Task GIVEN_ThereIsAnDeleteRequest_WHEN_DeletingACategory_THen_ShouldReturnOkResultAndCallServiceOnce()
+        {
+            //Arrange
+            var request = new CategoryDeleteRequest
+            {
+              Id =1
+            };
+            
+            //Act
+          
+            var actual = await _testing.Delete(request);
 
-//        [Fact]
-//        public async Task SaveUserAsync_ShouldSaveUserInformation()
-//        {
-//            // arrange
-//            byte[] arr1 = { 0, 100, 120, 210, 255 };
+            //Assert
+            _mockCategoryService
+                .Verify(service =>
+                        service.DeleteAsync(
+                            request.Id)
+                    );
+        }
 
-//            var insertRequest = new CategoryInsertRequest
-//            {
-//                Name = "test",
-//                Order = 5,
-//                Image = arr1
-//            };
-
-
-//            _mockCategoryService.Setup(cs => 
-//                cs.InsertAsync(
-//                    insertRequest.Name, 
-//                    insertRequest.Order, 
-//                    insertRequest.Image));
-
-//            // act
-//            await _testing.Insert(insertRequest);
-
-//            // assert
-//            _mockCategoryService.Verify(cs => 
-//                cs.InsertAsync(
-//                    insertRequest.Name,
-//                    insertRequest.Order,
-//                    insertRequest.Image), 
-//                Times.Once);
-//        }
-
-//    }
-//}
+    }
+}
