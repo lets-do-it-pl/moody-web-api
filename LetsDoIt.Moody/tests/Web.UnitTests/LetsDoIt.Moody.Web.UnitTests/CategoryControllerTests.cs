f7d4ee8bf94838af00f659e6447e62da82a723f9
ruveydacalis <sruveydacls@gmail.com>
2020-08-20 19:50:21 +0200

    tests

--- LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/CategoryControllerTests.cs
+++ LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/CategoryControllerTests.cs
@@ -24,54 +24,18 @@ namespace LetsDoIt.Moody.Web.UnitTests.Controllers
             _testing = new CategoryController(_mockCategoryService.Object);
         }
 
-        
-        [Fact]
-        public async Task GIVEN_ThereIsAnDeleteRequestWithoutId_WHEN_DeletingTheRequest_THEN_ShouldGetBadRequest()
-        {
-            CategoryDeleteRequest deleteRequest = null;
-
-            //Act
-            var actual = await _testing.Delete(deleteRequest);
-
-            //Assert
-            Assert.IsType<BadRequestResult>(actual);
-        }
-
-
         [Fact]
         public async Task GIVEN_ThereIsAnDeleteRequest_WHEN_DeletingTheRequest_THEN_ShouldGetOkResult()
         {
-            CategoryDeleteRequest deleteRequest = new CategoryDeleteRequest
-            {
-                Id = 1
-            };
+            int id = 3;
+
+            _mockCategoryService.Setup(c => c.DeleteAsync(id));
 
             //Act
-            var actual = await _testing.Delete(deleteRequest);
+            var actual = await _testing.Delete(id);
 
             //Assert
             Assert.IsType<OkResult>(actual);
+        
         }
-        [Fact]
-        public async Task GIVEN_ThereIsAnDeleteRequest_WHEN_DeletingACategory_THen_ShouldReturnOkResultAndCallServiceOnce()
-        {
-            //Arrange
-            var request = new CategoryDeleteRequest
-            {
-              Id =1
-            };
-            
-            //Act
-          
-            var actual = await _testing.Delete(request);
-
-            //Assert
-            _mockCategoryService
-                .Verify(service =>
-                        service.DeleteAsync(
-                            request.Id)
-                    );
-        }
-
-    }
-}
+        }}
