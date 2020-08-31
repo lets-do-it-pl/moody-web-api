114360443d89c5720a6ebcef3b8953753e886925
NotMuchLeft <ahmetkuris2308@gmail.com>
2020-08-31 08:55:40 +0200

    Added New Tests for Token Authorization

--- LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/Attributes/TokenAuthorizationAttributeTests.cs
+++ LetsDoIt.Moody/tests/Web.UnitTests/LetsDoIt.Moody.Web.UnitTests/Attributes/TokenAuthorizationAttributeTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using LetsDoIt.Moody.Web.Filters;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -12,42 +13,64 @@ using Xunit;
 
 namespace LetsDoIt.Moody.Web.UnitTests.Attributes
 {
+    using Application.User;
+
     public class TokenAuthorizationAttributeTests
     {
         private readonly TokenAuthorizationAttritbute _testing;
-       
-
+        private readonly ActionExecutingContext _actionExecutingContext;
+        private readonly ActionExecutedContext _context;
 
         public TokenAuthorizationAttributeTests()
         {
             _testing = new TokenAuthorizationAttritbute();
-        }
 
-        [Fact]
-        public async Task OnActionExecutionAsync_WhenTokenHeaderIsMissing_ShouldReturnUnauthorizedResult()
-        {
+             var defaultHttpContext = new DefaultHttpContext();
 
-            var defaultHttpContext = new DefaultHttpContext();
+             var actionContext = new ActionContext(
+                 defaultHttpContext,
+                 Mock.Of<RouteData>(),
+                 Mock.Of<ActionDescriptor>());
 
-            var actionContext = new ActionContext(
-                defaultHttpContext,
-                Mock.Of<RouteData>(),
-                Mock.Of<ActionDescriptor>());
-
-            var actionExecutingContext = new ActionExecutingContext(
+             _actionExecutingContext = new ActionExecutingContext(
                 actionContext,
                 new List<IFilterMetadata>(),
                 new Dictionary<string, object>(),
                 Mock.Of<Controller>()
             );
 
-            var context = new ActionExecutedContext(actionContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+              _context = new ActionExecutedContext(actionContext, new List<IFilterMetadata>(), Mock.Of<Controller>());
+        }
 
-            Task<ActionExecutedContext> Next() => Task.FromResult(context);
+        [Fact]
+        public async Task OnActionExecutionAsync_WhenTokenHeaderIsMissing_ShouldReturnUnauthorizedResult()
+        {
+            Task<ActionExecutedContext> Next() => Task.FromResult(_context);
 
-            await _testing.OnActionExecutionAsync(actionExecutingContext, Next);
+            await _testing.OnActionExecutionAsync(_actionExecutingContext, Next);
 
-            actionExecutingContext.Result.Should().BeOfType<UnauthorizedResult>();
+            _actionExecutingContext.Result.Should().BeOfType<UnauthorizedResult>();
+        }
+
+        [Fact]
+        public async Task OnActionExecutionAsync_WhenIsNotValid_ShouldReturnUnauthorizedResult()
+        {
+            var serviceProvider = new Mock<IServiceProvider>();
+            var userService = new Mock<IUserService>();
+
+            var token = "bad.token";
+
+            userService.Setup(us => us.IsTokenValidAsync(token)).ReturnsAsync(false);
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IUserService)))
+                .Returns(userService.Object);
+
+            Task<ActionExecutedContext> Next() => Task.FromResult(_context);
+
+            await _testing.OnActionExecutionAsync(_actionExecutingContext, Next);
+
+            _actionExecutingContext.Result.Should().BeOfType<UnauthorizedResult>();
         }
 
     }
