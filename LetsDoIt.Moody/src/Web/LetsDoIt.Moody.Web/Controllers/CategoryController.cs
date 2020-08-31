a97213935c2795182e72f81b385602c5b6a672fe
NotMuchLeft <ahmetkuris2308@gmail.com>
2020-08-31 08:56:41 +0200

    Revert "Revert "Added New Tests for Token Authorization""
    This reverts commit bf8cb716f9728c1ba5bf4c76f3ae5305054c071f.

--- LetsDoIt.Moody/src/Web/LetsDoIt.Moody.Web/Controllers/CategoryController.cs
+++ LetsDoIt.Moody/src/Web/LetsDoIt.Moody.Web/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Threading.Tasks;
 using System.Linq;
+using LetsDoIt.Moody.Application.User;
 using LetsDoIt.Moody.Web.Filters;
 
 namespace LetsDoIt.Moody.Web.Controllers
@@ -13,7 +14,7 @@ namespace LetsDoIt.Moody.Web.Controllers
 
     [ApiController] 
     [Route("api/categories")]
-    [TokenAuthorizationAttritbute]
+    [TokenAuthorizationAttritbute()]
     public class CategoryController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
