34c70bf2cf5ddc0878ba0d2ef585d23aa80942f4
NotMuchLeft <ahmetkuris2308@gmail.com>
2020-09-04 16:40:39 +0200

    Added integration test base

---
+++ LetsDoIt.Moody/tests/Application.IntegrationTests/LetsDoIt.Moody.Application.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Net.Http;
+using LetsDoIt.Moody.Persistance;
+using LetsDoIt.Moody.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LetsDoIt.Moody.Application.IntegrationTests
+{
+    public class IntegrationTestBase
+    {
+        protected readonly HttpClient TestClient;
+
+        protected IntegrationTestBase()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {   
+                    builder.ConfigureServices(services =>
+                    {
+                        var descriptor = services.SingleOrDefault(
+                            d => d.ServiceType ==
+                                 typeof(DbContextOptions<ApplicationContext>));
+
+                        services.Remove(descriptor);
+
+                        services.AddDbContext<ApplicationContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+
+            TestClient = appFactory.CreateClient();
+        }
+
+
+        //When Authorization attribute added will be needed
+
+        //protected async Task AuthorizeAsync()
+        //{
+        //}
+    }
+}
---
+++ LetsDoIt.Moody/tests/Application.IntegrationTests/LetsDoIt.Moody.Application.IntegrationTests/User/UserOperationsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LetsDoIt.Moody.Web.Entities.Requests;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace LetsDoIt.Moody.Application.IntegrationTests.User
+{
+    public class UserOperationsTests:IntegrationTestBase
+    {
+        [Fact]
+        public async Task SaveUser_GivenNoException_ShouldReturnCreated()
+        {
+            // Arrange
+            var saveUserRequest = new SaveUserRequest
+            {
+                Username = "good.username",
+                Password = "good.password"
+            };
+
+            var httpContent = new StringContent(JsonConvert.SerializeObject(saveUserRequest));
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            // Act
+            var response = await TestClient.PostAsync("/api/users",httpContent);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+    }
+}
