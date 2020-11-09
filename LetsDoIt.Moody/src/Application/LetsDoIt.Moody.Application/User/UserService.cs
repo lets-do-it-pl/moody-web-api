0df52c17bf27d18c15a8d15dc0cb99ee8ace5738
NotMuchLeft <ahmetkuris2308@gmail.com>
2020-11-09 21:36:36 +0100

    Refactor

--- LetsDoIt.Moody/src/Application/LetsDoIt.Moody.Application/User/UserService.cs
+++ LetsDoIt.Moody/src/Application/LetsDoIt.Moody.Application/User/UserService.cs
@@ -231,14 +231,12 @@ namespace LetsDoIt.Moody.Application.User
 
         private static async Task<string> ReadHtmlContent(string filePath)
         {
-            await using (FileStream fileStream =
+            await using FileStream fileStream =
                 new FileStream(AppDomain.CurrentDomain.BaseDirectory
                                + filePath,
-                    FileMode.Open))
-            {
-                using StreamReader streamReader = new StreamReader(fileStream, Encoding.Unicode);
-                return await streamReader.ReadToEndAsync();
-            }
+                    FileMode.Open);
+            using StreamReader streamReader = new StreamReader(fileStream, Encoding.Unicode);
+            return await streamReader.ReadToEndAsync();
         }
     }
 }
