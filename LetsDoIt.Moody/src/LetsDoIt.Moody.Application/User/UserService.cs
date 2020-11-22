91cb21ce5415665cd668c13be67496e4d8d8b354
NotMuchLeft <ahmetkuris2308@gmail.com>
2020-11-22 18:09:01 +0100

    Add Activate User

--- LetsDoIt.Moody/src/LetsDoIt.Moody.Application/User/UserService.cs
+++ LetsDoIt.Moody/src/LetsDoIt.Moody.Application/User/UserService.cs
@@ -1,4 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using LetsDoIt.MailSender;
+using LetsDoIt.Moody.Application.Constants;
+using LetsDoIt.Moody.Application.CustomExceptions;
+using LetsDoIt.Moody.Application.Security;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace LetsDoIt.Moody.Application.User
@@ -9,18 +18,34 @@ namespace LetsDoIt.Moody.Application.User
 
     public class UserService : IUserService
     {
+        private const string HtmlFilePath = @"\HtmlTemplates\EmailTokenVerification.html";
+        private const string EmailVerification = "Email Verification";
         private readonly ILogger<UserService> _logger;
         private readonly IRepository<User> _userRepository;
+        private readonly IMailSender _mailSender;
+        private readonly ISecurityService _securityService;
 
-        public UserService(ILogger<UserService> logger, IRepository<User> userRepository)
+        public UserService(ILogger<UserService> logger,
+            IRepository<User> userRepository,
+            IMailSender mailSender,
+            ISecurityService securityService)
         {
             _logger = logger;
             _userRepository = userRepository;
+            _mailSender = mailSender;
+            _securityService = securityService;
         }
 
         public async Task SaveUserAsync(string username, string password, string email, string name, string surname)
         {
-            var userEntity = new UserEntity(username,password,name,surname,email);
+            var isUserExisted = await _userRepository.Get().AnyAsync(u => u.Username == username && !u.IsDeleted);
+
+            if (isUserExisted)
+            {
+                throw new DuplicateNameException($"The username is already in the database. Username = {username}");
+            }
+
+            var userEntity = new UserEntity(username, password, name, surname, email);
 
             await _userRepository.AddAsync(new User
             {
@@ -31,14 +56,50 @@ namespace LetsDoIt.Moody.Application.User
             });
         }
 
-        public Task SendActivationEmailAsync(string referer, string email)
+        public async Task SendActivationEmailAsync(string referer, string email)
         {
-            throw new System.NotImplementedException();
+            var dbUser = await _userRepository.GetAsync(u => u.Email == email && !u.IsDeleted);
+
+            if (dbUser == null)
+            {
+                throw new EmailNotRegisteredException(email);
+            }
+
+            var content = await ReadHtmlContent(HtmlFilePath);
+
+            var frontEndUri = new Uri(referer);
+
+            var token = _securityService.GenerateJwtToken(dbUser.Id.ToString(), dbUser.FullName, UserTypeConstants.Standard);
+
+            await _mailSender.SendAsync(EmailVerification,
+                content.Replace("{{action_url}}",
+                    "http://" + frontEndUri.Host
+                              + "?token=" + token.Token),
+                email);
         }
 
-        public Task ActivateUser(int id)
+        public async Task ActivateUser(int id)
         {
-            throw new System.NotImplementedException();
+            var dbUser = await _userRepository.GetAsync(u => u.Id == id && !u.IsDeleted);
+
+            if (dbUser == null)
+            {
+                throw new UserNotFoundException(id);
+            }
+
+            await _userRepository.UpdateAsync(new User
+            {
+                IsActive = true
+            });
+        }
+
+        private static async Task<string> ReadHtmlContent(string filePath)
+        {
+            await using FileStream fileStream = new FileStream(AppDomain.CurrentDomain.BaseDirectory + filePath, FileMode.Open);
+
+            using StreamReader streamReader = new StreamReader(fileStream, Encoding.Unicode);
+
+            return await streamReader.ReadToEndAsync();
         }
     }
 }
