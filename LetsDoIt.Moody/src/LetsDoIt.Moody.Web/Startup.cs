42ead62475cf70897f6634f39bf177aaa7a90072
NotMuchLeft <ahmetkuris2308@gmail.com>
2020-12-12 12:14:26 +0100

    Refactor Startup

--- LetsDoIt.Moody/src/LetsDoIt.Moody.Web/Startup.cs
+++ LetsDoIt.Moody/src/LetsDoIt.Moody.Web/Startup.cs
@@ -1,32 +1,14 @@
-using System;
-using System.Text;
-using HealthChecks.UI.Client;
-using LetsDoIt.Moody.Application.Client;
-using LetsDoIt.Moody.Web.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using LetsDoIt.MailSender.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.IdentityModel.Tokens;
 
 namespace LetsDoIt.Moody.Web
 {
-    using Application.Category;
-    using Application.Constants;
-    using Application.Options;
-    using Application.Security;
-    using Application.VersionHistory;
+    using Extensions;
     using Middleware;
-    using Persistence;
-    using Persistence.Entities;
-    using Persistence.Repositories;
-    using Persistence.Repositories.Base;
-    using Persistence.Repositories.Category;
 
     public class Startup
     {
@@ -37,112 +19,23 @@ namespace LetsDoIt.Moody.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<JwtOptions>(Configuration.GetSection(JwtOptions.Jwt));
-
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-                .AddJwtBearer(options =>
-                {
-                    options.RequireHttpsMetadata = false;
-                    options.SaveToken = true;
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = Configuration["Jwt:Issuer"],
-                        ValidAudience = Configuration["Jwt:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:SecretKey"])),
-                        ClockSkew = TimeSpan.Zero,
-                        RequireExpirationTime = true
-                    };
-                });
-
-            services.AddAuthorization(config =>
-            {
-                config.AddPolicy(UserTypeConstants.Admin, Policies.AdminPolicy());
-                config.AddPolicy(UserTypeConstants.Standard, Policies.StandardPolicy());
-                config.AddPolicy(UserTypeConstants.Client, Policies.ClientPolicy());
-            });
-
-            services.AddResponseCompression();
-
-            var connectionString = Configuration.GetConnectionString("MoodyDBConnection");
-
-            services
-                .AddHealthChecks()
-                .AddSqlServer(connectionString, "SELECT 1", name: "SqlServerApplicationDb");
-
-            var url = Configuration.GetValue<string>("HealthChecksUri");
-
-            services.AddHealthChecksUI(s =>
-            {
-                s.AddHealthCheckEndpoint("Moody API", url);
-            })
-            .AddInMemoryStorage();
-
-            services.AddDbContext<ApplicationContext>(opt =>
-                opt
-                    .UseLazyLoadingProxies()
-                    .UseSqlServer(connectionString));
+            services.AddAuthenticationConfig(Configuration)
+                .AddAuthorizationConfig()
+                .AddResponseCompression()
+                .AddHealthCheckConfig(Configuration)
+                .AddDbContextConfig(Configuration)
+                .AddSwaggerConfig()
+                .AddMailSender()
+                .AddCustomClasses();
 
             services.AddControllers();
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Moody API",
-                    Version = "v1",
-                    Description = "Moody API details are here."
-                });
-
-                var securitySchema = new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.Http,
-                    Scheme = "bearer",
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                };
-                c.AddSecurityDefinition("Bearer", securitySchema);
-
-                var securityRequirement = new OpenApiSecurityRequirement
-                {
-                    {securitySchema, new[] {"Bearer"}}
-                };
-                c.AddSecurityRequirement(securityRequirement);
-            });
-
-            services.AddTransient<ICategoryRepository, CategoryRepository>();
-            services.AddTransient<IRepository<VersionHistory>, VersionHistoryRepository>();
-            services.AddTransient<IRepository<Client>, ClientRepository>();
-            services.AddTransient<IRepository<User>, UserRepository>();
-            services.AddTransient<IRepository<CategoryDetail>, CategoryDetailsRepository>();
-
-            services.AddTransient<ICategoryService, CategoryService>();
-            services.AddTransient<IVersionHistoryService, VersionHistoryService>();
-            services.AddTransient<IClientService, ClientService>();
-            services.AddSingleton<ISecurityService, SecurityService>();
+            services.Configure<SmtpOptions>(Configuration.GetSection(SmtpOptions.SmtpSectionName));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(
-            IApplicationBuilder app,
-            IWebHostEnvironment env,
-            ApplicationContext context)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -161,27 +54,15 @@ namespace LetsDoIt.Moody.Web
 
             app.UseRouting();
 
-            app.UseAuthentication();
+            app.UseAuthentication()
+                .UseAuthorization();
 
-            app.UseAuthorization();
-
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Moody API V1");
-            });
+            app.UseCustomSwaggerConfig();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHealthChecksUI();
-
-                endpoints.MapHealthChecks("/healthCheck", new HealthCheckOptions()
-                {
-                    Predicate = _ => true,
-                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-                });
+                endpoints.MapHealthChecksConfig();
             });
         }
     }
