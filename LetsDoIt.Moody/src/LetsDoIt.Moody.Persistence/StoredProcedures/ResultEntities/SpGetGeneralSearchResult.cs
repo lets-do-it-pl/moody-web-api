2efdcc0b69900379e6ed401190fd64fd0ddfcfa0
M7ZAHIT <47788535+M7ZAHIT@users.noreply.github.com>
2021-01-20 00:03:45 +0100

    MOODY-28-Stored-Procedure-For-AutoComplete-Search Changed field name Value with Type

--- LetsDoIt.Moody/src/LetsDoIt.Moody.Persistence/StoredProcedures/ResultEntities/SpGetGeneralSearchResult.cs
+++ LetsDoIt.Moody/src/LetsDoIt.Moody.Persistence/StoredProcedures/ResultEntities/SpGetGeneralSearchResult.cs
@@ -8,7 +8,7 @@ namespace LetsDoIt.Moody.Persistence.StoredProcedures.ResultEntities
 {
     public class SpGetGeneralSearchResult
     {
-        public string Name { get; set; }
-        public string Value { get; set; }
+        public string Name {get;set;}
+        public string Type {get;set;}
     }
 }
