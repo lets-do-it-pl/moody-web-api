f532418140c010a4531fbdeb182d2e88b8f96039
Salih Ozturk <salihozturk020@gmail.com>
2021-01-23 15:32:53 +0100

    MOODY-35-Unit-Tests-For-SearchService-And-SearchController add GetGeneralSearchResultAsync_ShouldThrownNullReferenceException_WhenSearchKeyIsNull

---
+++ LetsDoIt.Moody/tests/LetsDoIt.Moody.Application.UnitTests/Data/DataServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace LetsDoIt.Moody.Application.UnitTests.DataService
+{
+    using Data;
+    using Persistence;
+
+    public class DataServiceTests
+    {
+
+        private readonly Mock<IApplicationContext> _mockApplicationContext;
+        private readonly IDataService _testing;
+
+        public DataServiceTests()
+        {
+            _mockApplicationContext = new Mock<IApplicationContext>();
+            _testing = new DataService(_mockApplicationContext.Object);
+        }
+
+        [Fact]
+
+        public async Task GetGeneralSearchResultAsync_ShouldThrownNullReferenceException_WhenSearchKeyIsNull()
+        {
+            string searchKey = null;
+
+            async Task Test() => await _testing.SpGetGeneralSearchResultAsync(searchKey);
+
+            await Assert.ThrowsAsync<NullReferenceException>(Test);
+        }
+
+    }
+}
