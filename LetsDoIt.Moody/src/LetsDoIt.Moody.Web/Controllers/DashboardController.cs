cc84fe6d2e8fe15717c0914f72a672f90ff0cfbe
Saban Durna <sabandurna@gmail.com>
2021-01-23 04:53:20 +0100

    MOODY-40-refctoring-and-merging Refactor Search and Dashboard Controllers

--- LetsDoIt.Moody/src/LetsDoIt.Moody.Web/Controllers/DashboardController.cs
+++ LetsDoIt.Moody/src/LetsDoIt.Moody.Web/Controllers/DashboardController.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace LetsDoIt.Moody.Web.Controllers
 {
     using Application.Constants;
     using Application.Dashboard;
-    using LetsDoIt.Moody.Persistence.StoredProcedures.ResultEntities;
-    using LetsDoIt.Moody.Web.Entities.Responses;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
+    using Entities.Responses;
+    using Persistence.StoredProcedures.ResultEntities;
 
     [Route("api/[controller]")]
     [ApiController]
@@ -34,10 +34,11 @@ namespace LetsDoIt.Moody.Web.Controllers
             return Ok(dashboardResult.Select(ToDashboardItemsResponse));
         }
 
-        private static DashboardItemsResponse ToDashboardItemsResponse(SpGetDashboardItemsResult dashboardResult)
-        {  
-            return new DashboardItemsResponse { Name = dashboardResult.Name, TotalNumber = dashboardResult.TotalNumber };
-        }
+        private static DashboardItemsResponse ToDashboardItemsResponse(SpGetDashboardItemsResult dashboardResult) => new DashboardItemsResponse
+        {
+            Name = dashboardResult.Name,
+            TotalNumber = dashboardResult.TotalNumber
+        };
     } 
 }
 
