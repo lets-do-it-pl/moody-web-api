e4708c0cd746500ec6ce45297133ae5d99431141
NotMuchLeft <ahmetkuris2308@gmail.com>
2021-01-24 01:49:30 +0100

    MOODY-42-make-user-search-admin-only Add usertype parameter to general serach results

--- LetsDoIt.Moody/src/LetsDoIt.Moody.Application/Search/SearchService.cs
+++ LetsDoIt.Moody/src/LetsDoIt.Moody.Application/Search/SearchService.cs
@@ -3,28 +3,35 @@ using System.Collections.Generic;
 
 namespace LetsDoIt.Moody.Application.Search
 {
+    using Constants;
     using Data;
-    using LetsDoIt.Moody.Persistence.StoredProcedures.ResultEntities;
+    using Persistence.StoredProcedures.ResultEntities;
     using System.Linq;
 
     public class SearchService : ISearchService
     {
+        private const string CategoryName = "Category";
         private readonly IDataService _dataService;
         public SearchService(IDataService dataService)
         {
             _dataService = dataService;
         }
 
-        public async Task<ICollection<SpGetGeneralSearchResult>> GetGeneralSearchResultAsync(string searchKey)
+        public async Task<ICollection<SpGetGeneralSearchResult>> GetGeneralSearchResultAsync(string userType, string searchKey)
         {
             if (string.IsNullOrWhiteSpace(searchKey))
             {
                 return Enumerable.Empty<SpGetGeneralSearchResult>().ToList();
             }
 
-            return await _dataService.GetGeneralSearchResultAsync(searchKey.ToLower());
+            var results = await _dataService.GetGeneralSearchResultAsync(searchKey.ToLower());
+
+            if (userType == UserTypeConstants.Standard)
+            {
+                return results.Where(sggsr => sggsr.Name == CategoryName).ToArray();
+            }
+
+            return results;
         }
-
-
     }
 }
