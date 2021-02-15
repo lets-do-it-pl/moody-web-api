1a8087d99fb100a393a0b0c42c5783871703e56e
M7ZAHIT <47788535+M7ZAHIT@users.noreply.github.com>
2021-02-15 19:06:02 +0100

    Create Account Service And Account Response

---
+++ LetsDoIt.Moody/src/LetsDoIt.Moody.Application/Account/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsDoIt.Moody.Application.Account
+{
+    public interface IAccountService
+    {
+        Task<Persistence.Entities.User> GetAccount(int userId);
+
+        Task UpdateAccount(int userId);
+
+    }
+}
---
+++ LetsDoIt.Moody/src/LetsDoIt.Moody.Web/Entities/Responses/AccountResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsDoIt.Moody.Web.Entities.Responses
+{
+    public class AccountResponse
+    {
+    }
+}
