2ac0b853dee283a39a422dea70c2d045f92f4d11
Salih Ozturk <salihozturk@yahoo.com>
2021-03-01 16:00:36 +0100

    Add second test case

--- LetsDoIt.Moody/tests/LetsDoIt.Moody.Application.UnitTests/Data/DataServiceTests.cs
+++ LetsDoIt.Moody/tests/LetsDoIt.Moody.Application.UnitTests/Data/DataServiceTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Autofac.Extras.Moq;
 using EntityFrameworkExtras.EFCore;
 using LetsDoIt.Moody.Persistence.StoredProcedures;
 using LetsDoIt.Moody.Persistence.StoredProcedures.ResultEntities;
@@ -26,27 +29,57 @@ namespace LetsDoIt.Moody.Application.UnitTests.DataService
         }
 
 
-        
+
         [Fact]
         public async Task GetGeneralSearchResultAsync_ShouldThrowNullReferenceException_WhenSearchKeyIsNull()
         {
-
-            Assert.ThrowsAsync<NullReferenceException>(() => _testing.GetGeneralSearchResultAsync(""));
-            
+            await Assert.ThrowsAsync<NullReferenceException>(() => _testing.GetGeneralSearchResultAsync(""));
 
         }
 
-        //[Fact]
-        //public async Task GetGeneralSearchResultAsync_ShouldReturnSpGetResult_WhenResultExist()
-        //{
-        //    var entity = new SpGetGeneralSearchResult
-        //    {
-        //        Id = 1,
-        //        Name = "slaih",
-        //        Type = "aa"
-        //    };
-        //    _mockApplicationContext.Setup(x=>x.Database.ExecuteStoredProcedureAsync<SpGetGeneralSearchResult>())
-        //}
+        [Fact]
+        public async Task GetGeneralSearchResultAsync_ShouldReturnSpGetResult_WhenResultExist()
+        {
+            string searchKey = "salih";
+            var input = new SpGeneralSearch { SearchValue = searchKey };
+
+            _mockApplicationContext.Setup(x =>
+                    x.Database.ExecuteStoredProcedureAsync<SpGetGeneralSearchResult>(input, It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(GetSampleSearchResult());
+
+            var actual = _testing.GetGeneralSearchResultAsync(searchKey).Result.ToList();
+            var expectedList = GetSampleSearchResult().ToList();
+
+            Assert.Equal(expectedList[0].Name, actual[0].Name);
+
+        }
+
+        private IEnumerable<SpGetGeneralSearchResult> GetSampleSearchResult()
+        {
+            IEnumerable<SpGetGeneralSearchResult> list = new List<SpGetGeneralSearchResult>
+            {
+                new SpGetGeneralSearchResult
+                {
+                    Id = 1,
+                    Name = "salih",
+                    Type = "A"
+                },
+                new SpGetGeneralSearchResult
+                {
+                    Id = 1,
+                    Name = "burak",
+                    Type = "A"
+                },
+                new SpGetGeneralSearchResult
+                {
+                    Id = 1,
+                    Name = "Ali",
+                    Type = "A"
+                }
+            };
+            return list;
+        }
+
 
     }
 }
